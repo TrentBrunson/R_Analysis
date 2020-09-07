f2aa2c9f793e12bc2bbaf5c266e026df81377a06
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-07 16:34:33 -0500

    table mutations

--- 01_Demo/demo.R
+++ 01_Demo/demo.R
@@ -14,5 +14,18 @@ filter_table2 <- subset(demo_table2, price > 10000 & drive == "4wd" & "clean" %i
 demo_table[sample(1:nrow(demo_table), 3),]
 
 demo_table <- demo_table %>% mutate(Mileage_per_Year = Total_Miles/(2020-Year), IsActive = TRUE) #add columns to original data frame
-                                    
-                                    
+
+summarize_demo <- demo_table2 %>% group_by(condition) %>% summarize(Mean_Mileage = mean(odometer)) #create summary table
+
+summarize_demo <- demo_table2 %>% group_by(condition) %>% 
+  summarize(Mean_Mileage=mean(odometer),Maximum_Price=max(price),Num_Vehicles=n()) #create summary table with multiple columns
+
+demo_table3 <- read.csv('demo2.csv', check.names = F, stringsAsFactors = F)
+
+long_table <- gather(demo_table3,key="Metric",value="Score",buying_price:popularity)
+
+wide_table <- long_table %>% spread(key="Metric",value="Score")
+
+wide_table <- wide_table[,order(colnames(wide_table))]
+demo_table3 <- demo_table3[,order(colnames(demo_table3))]
+all.equal(demo_table3,wide_table)
