296ae9dc77ace2bac873faa2e625061adbea8272
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-28 19:06:00 -0500

    ANOVA test

--- 01_Demo/demo.R
+++ 01_Demo/demo.R
@@ -129,3 +129,19 @@ sample_table <- population_table %>% sample_n(50) #generate 50 randomly sampled 
 sample_table2 <- population_table %>% sample_n(50) #generate another 50 randomly sampled data points
 
 t.test(log10(sample_table$Miles_Driven),log10(sample_table2$Miles_Driven)) #compare means of two samples
+
+# Paired t-test
+mpg_data <- read.csv('mpg_modified.csv') #import dataset
+mpg_1999 <- mpg_data %>% filter(year==1999) #select only data points where the year is 1999
+mpg_2008 <- mpg_data %>% filter(year==2008) #select only data points where the year is 2008
+
+t.test(mpg_1999$hwy,mpg_2008$hwy,paired = T) #compare the mean difference between two samples
+
+# clean mt_cars data to change cyl numeric interval vector to categorical vector
+mtcars_filt <- mtcars[,c("hp","cyl")] #filter columns from mtcars dataset
+mtcars_filt$cyl <- factor(mtcars_filt$cyl) #convert numeric column to factor
+
+aov(hp ~ cyl,data=mtcars_filt) #compare means across multiple levels
+
+# wrapping ANOVA test in statistical function
+summary(aov(hp ~ cyl,data=mtcars_filt))
