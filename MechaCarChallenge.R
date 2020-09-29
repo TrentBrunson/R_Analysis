4a9f4a9a0db7900df9730aeb1da40fd92a9ffd3d
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-28 19:59:23 -0500

    mechaCar

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,5 @@
 # suspension coil test results from multiple production lots; weight capacity of multiple suspension coils were tested 
 # to determine if the manufacturing process is consistent across lots.
 
+MechaCar_MPG_Table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+View(MechaCar_Suspension_Table)
