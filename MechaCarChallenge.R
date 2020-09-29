75534475c9f84b8d97ffb85adcf8e66032d03796
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-28 20:14:20 -0500

    check probabilities

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,6 +9,14 @@
 # were collected for each vehicle.
 # suspension coil test results from multiple production lots; weight capacity of multiple suspension coils were tested 
 # to determine if the manufacturing process is consistent across lots.
+library(tidyverse)
 
-MechaCar_MPG_Table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCar_MPG_Table <- read.csv('MechaCar_mpg.csv',check.names=T)
 View(MechaCar_Suspension_Table)
+
+head(MechaCar_MPG_Table) #check out data in table
+
+# use multilinear regression model to check out probability values
+summary(lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD, data = MechaCar_MPG_Table))
+
+
